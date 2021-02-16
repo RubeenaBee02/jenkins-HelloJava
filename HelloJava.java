4d0837b3ae09cf5d9d53ac54c5cbb41d67fbbfe5
RubeenaBee02 <khanrubee02@gmail.com>
2021-02-16 12:58:37 +0530

    add hello java program

--- HelloJava.java
+++ HelloJava.java
@@ -1,7 +1,7 @@
 public class HelloJava{  
 public static void main(String[] args) {  
     //Code of Java for loop  
-    for(int i=1;i<=10;i++){  
+    for(int i=1;i<=50;i++){  
         System.out.println(i);  
     }  
 }  
